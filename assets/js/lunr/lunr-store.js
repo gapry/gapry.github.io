3af9459eb8b4e75b840f7398e97db1c2d4d0afea
gapry <gapry@users.noreply.github.com>
2025-03-28 14:15:41 +0000

    deploy: ad32e1590faf59ffeed3befea3c700953570b60b

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Test Post",
+        "excerpt":"Hi Hi !   #include &lt;iostream&gt; int main(void) {   std::cout &lt;&lt; \"I go to school by bus\\n\";   return 0; }  ","categories": [],
+        "tags": ["c++"],
+        "url": "/2025/03/28/test-post.html",
+        "teaser": null
+      }]
