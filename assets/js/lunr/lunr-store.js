a858e6352f5d37699a5ce1184a482a74b66c2623
gapry <gapry@users.noreply.github.com>
2025-03-28 14:37:32 +0000

    deploy: 9e75002a13c4039fe80a2efe8c6eda4c9c4f0b46

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "Test Post",
-        "excerpt":"Hi Hi !   #include &lt;iostream&gt; int main(void) {   std::cout &lt;&lt; \"I go to school by bus\\n\";   return 0; }  ","categories": [],
+        "title": "What is closure",
+        "excerpt":" #include &lt;iostream&gt;  int main(void) {   auto add = [](int a, int b) {     return a + b;   };   std::cout &lt;&lt; add(1, 2) &lt;&lt; \"\\n\";   return 0; }  ","categories": [],
         "tags": ["c++"],
-        "url": "/2025/03/28/test-post.html",
+        "url": "/2025/03/28/what-is-closure.html",
         "teaser": null
       }]
