9b733de6240e41c19f3752ab418232c6cc6c0df0
gapry <gapry@users.noreply.github.com>
2025-12-17 17:54:55 +0000

    deploy: 26fb84e76cedbafa6d663f9e11cda6f35d4999a0

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,1 @@
-var store = [{
-        "title": "What is closure",
-        "excerpt":"draft v1 The formula: \\(\\mathtt{(\\lambda\\ (a\\ b)\\ (+\\ a\\ b))}\\) 1 2 3 (define add (lambda (a b) (+ a b))) (display (add 1 2)) (newline) 1 2 3 4 5 6 7 8 9 #include &lt;iostream&gt; int main(void) { auto add = [](int a, int b) { return a...","categories": [],
-        "tags": ["c++"],
-        "url": "/2025/04/21/what-is-closure.html",
-        "teaser": null
-      }]
+var store = []
