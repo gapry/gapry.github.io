8b4201ade354ae29fa35e2b71fe719350f9ef02f
gapry <gapry@users.noreply.github.com>
2025-12-17 18:25:33 +0000

    deploy: 3f61309872769c22b2289b46bb84d4ec74d6e216

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Study Notes",
+        "excerpt":"draft v4 1 2 3 4 5 6 7 8 9 10 rm *.o ; g++ -O2 -c main.cpp -o main.o; llvm-objdump -d --x86-asm-syntax=att main.o main.o: file format elf64-x86-64 Disassembly of section .text.startup: 0000000000000000 &lt;main&gt;: 0: f3 0f 1e fa endbr64 4: 31 c0 xorl %eax, %eax 6: c3 retq...","categories": [],
+        "tags": ["compiler"],
+        "url": "/2025/12/17/notes.html",
+        "teaser": null
+      }]
