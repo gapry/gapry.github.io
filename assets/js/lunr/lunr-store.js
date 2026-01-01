7b6436a429b4770f4bbb6a2e40f55610fd1ccc06
gapry <gapry@users.noreply.github.com>
2026-01-01 02:08:06 +0000

    deploy: ae7d9bbd267ce26268c28a09d241b6f3db178810

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "Study Notes",
-        "excerpt":"draft v4 1 2 3 4 5 6 7 8 9 10 rm *.o ; g++ -O2 -c main.cpp -o main.o; llvm-objdump -d --x86-asm-syntax=att main.o main.o: file format elf64-x86-64 Disassembly of section .text.startup: 0000000000000000 &lt;main&gt;: 0: f3 0f 1e fa endbr64 4: 31 c0 xorl %eax, %eax 6: c3 retq...","categories": [],
+        "title": "Study Notes: Why xor eax, eax? ",
+        "excerpt":"Study Notes: Why xor eax, eax? These notes are based on the post Why xor eax, eax?, which is Day 1 of the Advent of Compiler Optimisations 2025 Series by Matt Godbolt. Development Environment $ lsb_release -d Description: Ubuntu 24.04.3 LTS $ gcc -v gcc version 13.3.0 $ clang -v...","categories": [],
         "tags": ["compiler"],
-        "url": "/2025/12/17/notes.html",
+        "url": "/2026/01/01/Advent-of-Compiler-Optimisations-Study-Notes-01.html",
         "teaser": null
       }]
