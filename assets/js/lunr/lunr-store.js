9bba7da38339fef1fda53f6911d5733c6a2a051c
gapry <gapry@users.noreply.github.com>
2026-02-27 16:35:36 +0000

    deploy: d468d04cfc5bb0e2d3f4ee3a67a3e875fe31a0e9

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "Study Notes: Why xor eax, eax? ",
-        "excerpt":"Study Notes: Why xor eax, eax? These notes are based on the post Why xor eax, eax?, which is Day 1 of the Advent of Compiler Optimisations 2025 Series by Matt Godbolt. Development Environment $ lsb_release -d Description: Ubuntu 24.04.3 LTS $ gcc -v gcc version 13.3.0 $ clang -v...","categories": [],
+        "title": "Study Notes: Why xor eax, eax?, Advent of Compiler Optimisations 2025",
+        "excerpt":"Study Notes: Why xor eax, eax?, Advent of Compiler Optimisations 2025 These notes are based on the post Why xor eax, eax?, which is Day 1 of the Advent of Compiler Optimisations 2025 Series by Matt Godbolt. Development Environment $ lsb_release -d Description: Ubuntu 24.04.3 LTS $ gcc -v gcc...","categories": [],
         "tags": ["compiler"],
         "url": "/2026/01/01/Advent-of-Compiler-Optimisations-Study-Notes-01.html",
         "teaser": null
