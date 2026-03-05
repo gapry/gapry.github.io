e805abca4f60d691964c20dbcd7374d57c810adf
gapry <gapry@protonmail.com>
2026-03-05 17:23:21 +0800

    blog: add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <footer className="site-footer">
+      <div className="footer-content">
+        <p>
+          My Technical Writing Licensed under{' '}
+          <a 
+            href="https://creativecommons.org/licenses/by-nc-sa/4.0/" 
+            target="_blank" 
+            rel="noopener noreferrer"
+          >
+            CC BY-NC-SA 4.0
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+}
