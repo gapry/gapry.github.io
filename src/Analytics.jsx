1d51ebddbb876c5b102994844ffad41716b965d4
gapry <gapry@protonmail.com>
2026-03-05 09:00:03 +0800

    blog: switch to React from Jekyll: update

--- src/Analytics.jsx
+++ src/Analytics.jsx
@@ -5,29 +5,33 @@ export default function Analytics() {
   const trackingId = siteConfig.analytics.trackingId;
 
   useEffect(() => {
-    if (!trackingId || document.getElementById('google-analytics')) {
+    if (!trackingId) {
       return;
     }
 
-    const script = document.createElement('script');
-    
-    script.id    = 'google-analytics';
-    script.src   = `https://www.googletagmanager.com/gtag/js?id=${trackingId}`;
-    script.async = true;
-    
-    document.head.appendChild(script);
-
     window.dataLayer = window.dataLayer || [];
-    
-    window.gtag = function() { 
-      window.dataLayer.push(arguments); 
-    };
+    if (typeof window.gtag !== 'function') {
+      window.gtag = function() {
+        window.dataLayer.push(arguments);
+      };
+      window.gtag('js', new Date());
+    }
 
-    window.gtag('js', new Date());
-    window.gtag('config', trackingId, { 
-      anonymize_ip: siteConfig.analytics.anonymizeIp 
+    window.gtag('config', trackingId, {
+      anonymize_ip: siteConfig.analytics.anonymizeIp,
     });
-  }, [trackingId]);
+
+    if (document.getElementById('google-analytics')) {
+      return;
+    }
+
+    const script  = document.createElement('script');
+    script.id     = 'google-analytics';
+    script.src    = `https://www.googletagmanager.com/gtag/js?id=${trackingId}`;
+    script.async  = true;
+
+    document.head.appendChild(script);
+  }, [trackingId, siteConfig.analytics.anonymizeIp]);
 
   return null; 
 }
