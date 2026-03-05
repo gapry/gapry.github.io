e588589db002dae0d410c75b5db686a52274f2e7
gapry <gapry@protonmail.com>
2026-03-05 11:22:46 +0800

    blog: support Markdown rendering and 404 routes (#38)

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  build: {
+    emptyOutDir: true,
+  },
+
   plugins: [react()],
+
   base: '/', 
+  
   server: {
     open: true, 
     port: 3000, 
