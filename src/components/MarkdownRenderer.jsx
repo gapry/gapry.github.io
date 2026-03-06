da124e78cdf4656399af85aab62aede91b4c8d1f
gapry <gapry@protonmail.com>
2026-03-06 08:09:02 +0800

    blog: code refactoring

---
+++ src/components/MarkdownRenderer.jsx
@@ -0,0 +1,42 @@
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+export default function MarkdownRenderer({ content, showBackLink = true }) {
+  return (
+    <article className="markdown-body">
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+              <SyntaxHighlighter
+                style={vscDarkPlus}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              >
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          }
+        }}
+      >
+        {content}
+      </ReactMarkdown>
+      
+      {showBackLink && (
+        <>
+          <hr />
+          <a href="/" className="back-link">← Back to Home</a>
+        </>
+      )}
+    </article>
+  );
+}
