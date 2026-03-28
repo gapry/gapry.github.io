39c4f9e3a1405267da5f9a0f143a7742e7a0259c
gapry <gapry@protonmail.com>
2026-03-28 21:52:57 +0800

    MarkdownRenderer: code refactoring

--- src/components/MarkdownRenderer.jsx
+++ src/components/MarkdownRenderer.jsx
@@ -7,7 +7,7 @@ const CodeBlock = ({ inline, className, children, ...props }) => {
   const match = /language-(\w+)/.exec(className || '');
 
   if (!inline && match) {
-    const language = match ? match[1] : '';
+    const language = match[1];
     const content  = String(children).replace(/\n$/, '');
 
     return (
